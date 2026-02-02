696a9723cf9433e17a9872ca8d8fc015d9d63e90
YampolMendoza <polmendoza663@gmail.com>
2026-02-02 18:47:09 -0500

    Remove init route

--- server.js
+++ server.js
@@ -19,29 +19,6 @@ app.get("/healthz", (req, res) => {
   res.status(200).send("OK");
 });
 
-// CREAR TABLA (solo una vez)
-app.get("/init", async (req, res) => {
-  try {
-    await pool.query(`
-      CREATE TABLE IF NOT EXISTS alquileres (
-        id SERIAL PRIMARY KEY,
-        tipo TEXT,
-        distrito TEXT,
-        direccion TEXT,
-        piso TEXT,
-        precio INTEGER,
-        condiciones TEXT,
-        telefono TEXT,
-        fecha TEXT
-      )
-    `);
-    res.send("Tabla creada ✅");
-  } catch (err) {
-    console.error(err);
-    res.status(500).send("Error creando tabla");
-  }
-});
-
 app.listen(PORT, () => {
   console.log(`🚀 Servidor en puerto ${PORT}`);
 });
